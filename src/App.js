fdd712db1c7005de7c418a8d02a953df42d93569
Elijah Sorensen <57324970+soreelij@users.noreply.github.com>
2023-04-16 18:43:42 -0600

    Loading image working (sort of)

--- src/App.js
+++ src/App.js
@@ -6,7 +6,16 @@ import React from 'react';
 
 function App() {
  return(
-    <ImageDisplay />
+  <div className="App">
+   <div className="container h-50">
+    <div className="row h-50 justify-content-center align-items-center">
+     <header className="App-header">
+      <h1>Album.AI</h1>
+      <ImageDisplay />
+     </header>
+    </div>
+   </div>
+  </div>
  )
 }
 
--- src/ImageDisplay.js
+++ src/ImageDisplay.js
@@ -1,13 +1,20 @@
 import axios from 'axios';
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {Bars} from 'react-loader-spinner';
 
 export default class ImageDisplay extends Component {
     state = {
-        image: null
+        image: null,
+        generating: false
     }
 
     handleClick = () => {
-        var self = this;
+        let self = this;
+
+        self.setState({
+            generating: true
+        });
+
         axios({
             method: 'post',
             url: 'http://127.0.0.1:7860/sdapi/v1/txt2img',
@@ -16,36 +23,62 @@ export default class ImageDisplay extends Component {
                 "steps": 1
             }
         })
-        .then(function (response) {
-          console.log(response);
-          var rawImage = response.data.images[0];
-          self.setState({
-              image: "data:image/png;base64," + rawImage
-          }, () =>  {console.log("Image data: " + self.state.image)})
-          console.log("rendering image...");
+            .then(function (response) {
+                console.log(response);
+                var rawImage = response.data.images[0];
+                self.setState({
+                    image: "data:image/png;base64," + rawImage,
+                    generating: false
+                }, () => {
+                    console.log("Image data: " + self.state.image)
+                })
+                console.log("rendering image...");
 
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
     }
 
     render() {
 
-        return(
-            <div className="App">
-            <p>Let's see if we can't generate and display an image...</p>
-            <button onClick={() => this.handleClick()}>Generate image</button>
-            <br />
-            {this.state.image ?
-                <div>
-                    <p>Here is the image:</p>
-                    <img src={ this.state.image } />
-                </div> :
-                <p>No image data</p>
-            }
-        </div>
+        return (
+            <div>
+                <div className="row">
+                    { this.state.image ?
+                        <div>
+                            <img src={this.state.image}/>
+                        </div> :
+                        this.state.generating ?
+                        <div>
+                            <br />
+                            <br />
+                            <br />
+                        </div> :
+                        <div className="align-items-center justify-content-center">
+                            <p>Generate an album cover below...</p>
+                            <button onClick={() => this.handleClick()}>Generate image</button>
+                        </div>
+                    }
+                    <div className="col-md">
+                        <Bars
+                            height="50"
+                            width="50"
+                            color="#4fa94d"
+                            ariaLabel="bars-loading"
+                            wrapperStyle={{
+                                position: "fixed",
+                                top: "50%",
+                                left: "50%",
+                                transform: "translate(-50%, -50%)"
+                            }}
+                            wrapperClass=""
+                            visible={!this.state.image && this.state.generating}
+                        />
+                    </div>
+                </div>
+            </div>
         )
     }
-        
+
 }
