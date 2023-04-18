ac895f5d79dc04303332ad4fe29080b20cfaf558
Natalie Hahle <nataliehahle@u.boisestate.edu>
2023-04-18 14:32:01 -0600

    working on natalies

--- src/stable-diffusion-webui/backend-start.sh
+++ src/stable-diffusion-webui/backend-start.sh
@@ -3,16 +3,16 @@ if [ "$(whoami)" == "elijahsorensen" ];
 then
     source ~/miniforge3/etc/profile.d/conda.sh
     export PYENCHANT_LIBRARY_PATH=/opt/homebrew/lib/libenchant-2.dylib
+    conda activate sd-env
 fi
 
 # Intel specific
 if [ "$(whoami)" == "nataliehahle" ];
 then
-    source ~/anaconda/etc/profile.d/conda.sh
+    source ~/sd-env/bin/activate
 fi
 
 # Activate the conda environment
-conda activate sd-env
 cd src/stable-diffusion-webui
 
 delimiter="################################################################"
@@ -22,4 +22,4 @@ printf "\e[1m\e[32mRunning AlgoRhythm backend...\n"
 printf "\e[1m\e[34mEli Sorensen & Natalie Hahle\e[0m"
 printf "\n%s\n" "${delimiter}"
 
-python launch.py  --skip-torch-cuda-test --no-half --use-cpu all --api
+python3 launch.py  --skip-torch-cuda-test --no-half --use-cpu all --api
